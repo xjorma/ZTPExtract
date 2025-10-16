d0fa30bf9b858432ba001be774d90ca5996136bc
David Gallardo <xjormaeko@gmail.com>
2025-10-16 12:31:03 -0400

    [ArcTool] Migrate to python 3.11 (using chatgpt) and make a simple exemple

--- ArcTool/batch_tool.py
+++ ArcTool/batch_tool.py
@@ -1,9 +1,29 @@
 import sys
-import arctool
+import os
+import ARCTool
 
-archive_path = r"D:\ztp_work\res\Stage\D_MN01A\STG_00.arc"
-destination  = r"D:\temp\test"
+src  = "D:/ztp_work/res/Stage/D_MN01A/STG_00.arc"
+temp = "./temp.arc"        # intermediate uncompressed archive
+out2 = "D:/temp/STG_00_extracted"      # folder for final extraction
 
-# simulate CLI: python arctool.py -o <destination> <archive_path>
-sys.argv = ["arctool", "-o", destination, archive_path]
-arctool.main()
+print(f"cwd: {os.getcwd()}")
+
+saved_cwd = os.getcwd()
+
+# pass 1: unyaz to file
+sys.argv = ["ARCTool", "-o", temp, src]
+ARCTool.main()
+
+# pass 2: extract uncompressed archive to folder
+sys.argv = ["ARCTool", "-o", out2, temp]
+ARCTool.main()
+
+os.chdir(saved_cwd)
+# delete intermediate file
+
+print(f"cwd: {os.getcwd()}")
+
+if os.path.exists(temp):
+    os.remove(temp)
+else:
+    print(f"The file {temp} does not exist")
